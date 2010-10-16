django-nonrel/django-nonrel

commit 2097284f20ced8903fdfceb601f60c22acff5c30

Fixed #13568 -- Fixed the blocktrans tag to not raise a KeyError if the number of variables in the singular and the plural block differ. Thanks, deloide.
git-svn-id: http://code.djangoproject.com/svn/django/trunk@13967 bcc190cf-cafb-0310-a4f2-bffc1f526a37

django/templatetags/i18n.py
@@ -76,8 +76,10 @@ class BlockTranslateNode(Node):
         if self.plural and self.countervar and self.counter:
             count = self.counter.resolve(context)
             context[self.countervar] = count
-            plural, vars = self.render_token_list(self.plural)
+            plural, plural_vars = self.render_token_list(self.plural)
             result = translation.ungettext(singular, plural, count)
+            if count != 1:
+                vars = plural_vars
         else:
             result = translation.ugettext(singular)
         # Escape all isolated '%' before substituting in the context.

tests/regressiontests/templates/tests.py
@@ -1116,6 +1116,9 @@ class Templates(unittest.TestCase):
             'i18n24': ("{% load i18n %}{% trans 'Page not found'|upper %}", {'LANGUAGE_CODE': 'de'}, u'SEITE NICHT GEFUNDEN'),
             'i18n25': ('{% load i18n %}{% trans somevar|upper %}', {'somevar': 'Page not found', 'LANGUAGE_CODE': 'de'}, u'SEITE NICHT GEFUNDEN'),
 
+            # translation of plural form with extra field in singular form (#13568)
+            'i18n26': ('{% load i18n %}{% blocktrans with myextra_field as extra_field count number as counter %}singular {{ extra_field }}{% plural %}plural{% endblocktrans %}', {'number': 1, 'myextra_field': 'test'}, "singular test"),
+
             ### HANDLING OF TEMPLATE_STRING_IF_INVALID ###################################
 
             'invalidstr01': ('{{ var|default:"Foo" }}', {}, ('Foo','INVALID')),

commit 3eaf069bbd66d8ce3076b5d0f23461feb3813a2c

Fixed #14126 -- Fixed an issue with changes to the blocktrans tag introduced in r13967 related to multiple plural forms. Thanks, mark0978, svetlyak40wt and Ramiro.
git-svn-id: http://code.djangoproject.com/svn/django/trunk@14239 bcc190cf-cafb-0310-a4f2-bffc1f526a37

django/templatetags/i18n.py
@@ -78,8 +78,7 @@ class BlockTranslateNode(Node):
             context[self.countervar] = count
             plural, plural_vars = self.render_token_list(self.plural)
             result = translation.ungettext(singular, plural, count)
-            if count != 1:
-                vars = plural_vars
+            vars.extend(plural_vars)
         else:
             result = translation.ugettext(singular)
         # Escape all isolated '%' before substituting in the context.

tests/regressiontests/templates/tests.py
@@ -1119,6 +1119,9 @@ class Templates(unittest.TestCase):
             # translation of plural form with extra field in singular form (#13568)
             'i18n26': ('{% load i18n %}{% blocktrans with myextra_field as extra_field count number as counter %}singular {{ extra_field }}{% plural %}plural{% endblocktrans %}', {'number': 1, 'myextra_field': 'test'}, "singular test"),
 
+            # translation of singular form in russian (#14126)
+            'i18n27': ('{% load i18n %}{% blocktrans count number as counter %}1 result{% plural %}{{ counter }} results{% endblocktrans %}', {'number': 1, 'LANGUAGE_CODE': 'ru'}, u'1 \u0440\u0435\u0437\u0443\u043b\u044c\u0442\u0430\u0442'),
+
             ### HANDLING OF TEMPLATE_STRING_IF_INVALID ###################################
 
             'invalidstr01': ('{{ var|default:"Foo" }}', {}, ('Foo','INVALID')),